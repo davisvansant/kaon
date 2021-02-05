635d70f2afcfa800287fb4e0721d8f88b894fff4
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-02-05 14:23:28 -0800

    initial collect_event, add new processed field, adjust handler, more structure for event loop, initial decay test

--- src/core.rs
+++ src/core.rs
@@ -14,6 +14,7 @@ pub struct Kaon {
     pub in_flight: bool,
     pub environment: std::env::VarsOs,
     pub api: Api,
+    pub processed: Vec<Context>,
 }
 
 impl Kaon {
@@ -81,13 +82,30 @@ impl Kaon {
         }
     }
 
+    #[instrument]
+    async fn collect_event(&mut self, new_event: Context) {
+        let event = self.processed.last();
+        if let Some(last_event) = event {
+            if last_event.aws_request_id.as_str() == new_event.aws_request_id.as_str() {
+                self.stop();
+                info!("| kaon collect event | event has already been processed!");
+            } else {
+                self.processed.push(new_event);
+                info!("| kaon collect event | event processed!");
+            }
+        } else {
+            self.processed.push(new_event);
+            info!("| kaon collect event | event processed!");
+        }
+    }
+
     // #[instrument]
     // async fn handler<F: Fn() + std::fmt::Debug>(f: F) {
     //     info!("| kaon handler | Kaon is invoking function!");
     //     f();
     // }
-    async fn handler<F: Fn()>(f: F) {
-        f();
+    async fn handler<Handler: Fn(Body, Context)>(handle: Handler, event: Body, context: Context) {
+        handle(event, context);
     }
 
     #[instrument]
@@ -103,51 +121,130 @@ impl Kaon {
             in_flight: false,
             environment: std::env::vars_os(),
             api,
+            processed: Vec::with_capacity(20),
         }
     }
 
     // #[instrument]
-    pub async fn decay<F: Fn() + Copy + std::fmt::Display + std::fmt::Debug>(
-        &mut self,
-        function: F,
-    ) {
+    // pub async fn decay<
+    //     Handler: Fn() + Copy + std::fmt::Display + std::fmt::Debug + std::ops::Fn(Body, Context) -> (),
+    // >(
+    //     &mut self,
+    //     function: Handler,
+    // ) {
+    pub async fn decay<E, C>(&mut self, function: fn(E, C)) {
         self.in_flight = true;
+
+        println!("{:?}", self.in_flight);
 
         // info!("| kaon decay | Kaon decay is in process ...");
 
-        if self.in_flight {
-            loop {
-                let event = self.api.runtime_next_invocation().await;
+        // if self.in_flight {
+        //     loop {
+        //         let event = self.api.runtime_next_invocation().await;
+        //
+        //         // match event {
+        //         //     Ok(event_response) => {
+        //         //         let headers = event_response.headers();
+        //         //         Api::set_tracing_header(headers).await;
+        //         //         let id = &headers.get("Lambda-Runtime-Aws-Request-Id").unwrap();
+        //         //         let arn = &headers.get("Lambda-Runtime-Invoked-Function-Arn").unwrap();
+        //         //         let identity = &headers.get("Lambda-Runtime-Cognito-Identity").unwrap();
+        //         //         let client = &headers.get("Lambda-Runtime-Client-Context").unwrap();
+        //         //         let context = Context::create(
+        //         //             id.to_str().unwrap().to_string(),
+        //         //             arn.to_str().unwrap().to_string(),
+        //         //             identity.to_str().unwrap().to_string(),
+        //         //             client.to_str().unwrap().to_string(),
+        //         //         )
+        //         //         .await;
+        //         //         let invoke_handler =
+        //         //             Self::handler(function, event_response.into_body(), context.clone())
+        //         //                 .await;
+        //         //         let fake_body = Body::from("more to come...");
+        //         //         let handle_response = self
+        //         //             .api
+        //         //             .runtime_invocation_response(context.aws_request_id.as_str(), fake_body)
+        //         //             .await;
+        //         //     }
+        //         //     Err(_) => {}
+        //         if let Ok(event_response) = event {
+        //             let headers = event_response.headers();
+        //             Api::set_tracing_header(headers).await;
+        //             let id = &headers.get("Lambda-Runtime-Aws-Request-Id").unwrap();
+        //             let arn = &headers.get("Lambda-Runtime-Invoked-Function-Arn").unwrap();
+        //             let identity = &headers.get("Lambda-Runtime-Cognito-Identity").unwrap();
+        //             let client = &headers.get("Lambda-Runtime-Client-Context").unwrap();
+        //             let context = Context::create(
+        //                 id.to_str().unwrap().to_string(),
+        //                 arn.to_str().unwrap().to_string(),
+        //                 identity.to_str().unwrap().to_string(),
+        //                 client.to_str().unwrap().to_string(),
+        //             )
+        //             .await;
+        //             // Self::handler(function, event_response.into_body(), context.clone()).await;
+        //             let fake_body = Body::from("more to come...");
+        //             let handle_response = self
+        //                 .api
+        //                 .runtime_invocation_response(context.aws_request_id.as_str(), fake_body)
+        //                 .await;
+        //             if handle_response.is_ok() {
+        //                 println!("event processed!");
+        //                 // continue;
+        //                 self.stop();
+        //             } else {
+        //                 println!("there was an error, shutting down");
+        //                 // break;
+        //                 self.stop();
+        //             }
+        //         } else {
+        //             println!("there was an error, shutting down");
+        //             // break;
+        //             self.stop();
+        //         }
+        //     }
+        // }
+        while self.in_flight {
+            let event = self.api.runtime_next_invocation().await;
 
-                match event {
-                    Ok(event_response) => {
-                        let headers = &event_response.headers();
-                        Api::set_tracing_header(headers).await;
-                        let id = &headers.get("Lambda-Runtime-Aws-Request-Id").unwrap();
-                        let arn = &headers.get("Lambda-Runtime-Invoked-Function-Arn").unwrap();
-                        let identity = &headers.get("Lambda-Runtime-Cognito-Identity").unwrap();
-                        let client = &headers.get("Lambda-Runtime-Client-Context").unwrap();
-                        let context = Context::create(
-                            id.to_str().unwrap().to_string(),
-                            arn.to_str().unwrap().to_string(),
-                            identity.to_str().unwrap().to_string(),
-                            client.to_str().unwrap().to_string(),
-                        )
+            if let Ok(event_response) = event {
+                let headers = event_response.headers();
+                Api::set_tracing_header(headers).await;
+                let id = &headers.get("Lambda-Runtime-Aws-Request-Id").unwrap();
+                let arn = &headers.get("Lambda-Runtime-Invoked-Function-Arn").unwrap();
+                let identity = &headers.get("Lambda-Runtime-Cognito-Identity").unwrap();
+                let client = &headers.get("Lambda-Runtime-Client-Context").unwrap();
+                let context = Context::create(
+                    id.to_str().unwrap().to_string(),
+                    arn.to_str().unwrap().to_string(),
+                    identity.to_str().unwrap().to_string(),
+                    client.to_str().unwrap().to_string(),
+                )
+                .await;
+                self.collect_event(context.clone()).await;
+                // checkpoint to see if we want to continue processing
+                while self.in_flight {
+                    let fake_body = Body::from("more to come...");
+                    let handle_response = self
+                        .api
+                        .runtime_invocation_response(context.aws_request_id.as_str(), fake_body)
                         .await;
-                        let invoke_handler = Self::handler(function).await;
-                        let fake_body = Body::from("more to come...");
-                        let handle_response = self
-                            .api
-                            .runtime_invocation_response(&context.aws_request_id, fake_body)
-                            .await;
+                    if handle_response.is_ok() {
+                        println!("event processed!");
+                        break;
+                    } else {
+                        println!("handle response was not ok");
+                        self.stop();
                     }
-                    Err(_) => {}
                 }
+            } else {
+                println!("error connecting to api");
+                self.stop();
             }
         }
     }
 
-    pub async fn stop(&mut self) {
+    pub fn stop(&mut self) {
         self.in_flight = false;
         info!("| kaon decay | Kaon decay stopped ...");
     }
@@ -298,8 +395,9 @@ mod tests {
             test_client_context,
         )
         .await;
-        // let test_event = || println!("test kaon event!");
-        // Kaon::handler(test_event, &test_context).await;
+        let test_event = |_, _| println!("test kaon event!");
+        let test_body = Body::from("test");
+        Kaon::handler(test_event, test_body, test_context).await;
     }
 
     #[tokio::test]
@@ -313,16 +411,78 @@ mod tests {
         assert_eq!(kaon.api.runtime_api.is_empty(), false);
     }
 
-    // #[tokio::test]
-    // async fn decay() {
-    //     let test_aws_lambda_runtime_api = mockito::server_address().to_string();
-    //     std::env::set_var("AWS_LAMBDA_RUNTIME_API", test_aws_lambda_runtime_api);
-    //     let mut kaon = Kaon::charge().await;
-    //     assert_eq!(kaon.in_flight, false);
-    //     let test_event = || println!("test kaon event!");
-    //     kaon.decay(test_event).await;
-    //     assert_eq!(kaon.in_flight, true);
-    //     kaon.stop();
-    //     assert_eq!(kaon.in_flight, false);
-    // }
+    #[tokio::test]
+    async fn decay() {
+        let test_aws_lambda_runtime_api = mockito::server_address().to_string();
+        std::env::set_var("AWS_LAMBDA_RUNTIME_API", test_aws_lambda_runtime_api);
+        let test_aws_request_id = String::from("8476a536-e9f4-11e8-9739-2dfe598c3fcd");
+        let test_arn =
+            String::from("arn:aws:lambda:us-east-2:123456789012:function:custom-runtime");
+        let test_identity = String::from("test_identity");
+        let test_client_context = String::from("test_client_context");
+
+        let test_context = Context::create(
+            test_aws_request_id,
+            test_arn,
+            test_identity,
+            test_client_context,
+        )
+        .await;
+        let test_body = Body::from("test");
+        let mock = mockito::mock("GET", "/2018-06-01/runtime/invocation/next")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_header(
+                "Lambda-Runtime-Aws-Request-Id",
+                "8476a536-e9f4-11e8-9739-2dfe598c3fcd",
+            )
+            .with_header(
+                "Lambda-Runtime-Trace-Id",
+                "Root=1-5bef4de7-ad49b0e87f6ef6c87fc2e700;Parent=9a9197af755a6419;Sampled=1",
+            )
+            .with_header(
+                "Lambda-Runtime-Client-Context",
+                "test_aws_mobile_sdk_client_and_device",
+            )
+            .with_header(
+                "Lambda-Runtime-Cognito-Identity",
+                "test_aws_module_sdk_cognito_idp",
+            )
+            .with_header("Lambda-Runtime-Deadline-Ms", "1542409706888")
+            .with_header(
+                "Lambda-Runtime-Invoked-Function-Arn",
+                "arn:aws:lambda:us-east-2:123456789012:function:custom-runtime",
+            )
+            .with_body(r#"{"test": "kaon"}"#)
+            .expect(2)
+            .create();
+        let mock_post = mockito::mock(
+            "POST",
+            "/2018-06-01/runtime/invocation/8476a536-e9f4-11e8-9739-2dfe598c3fcd/response",
+        )
+        .match_body("more to come...")
+        .expect(1)
+        .create();
+        // let test_event = |_, _| println!("test kaon event!");
+        // let test_event = |test_body, test_context| {
+        //     println!("test kaon event!");
+        // };
+        fn test_event(test_body: Body, test_context: Context) {
+            println!("kaon test {:?}", test_body);
+            println!("kaon test {}", test_context.aws_request_id);
+        }
+        // let tester = || test_event(test_body, test_context);
+        // let tester = || println!("test kaon");
+        let mut kaon = Kaon::charge().await;
+        assert_eq!(kaon.in_flight, false);
+        // kaon.decay(test_event(test_body, test_context)).await;
+        kaon.decay(test_event).await;
+        // assert_eq!(kaon.in_flight, true);
+        mock.assert();
+        assert!(mock.matched());
+        mock_post.assert();
+        assert!(mock_post.matched());
+        kaon.stop();
+        assert_eq!(kaon.in_flight, false);
+    }
 }
