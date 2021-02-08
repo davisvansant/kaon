0135f22a79a241c7a13a0802f51b2a787178aec2
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-02-08 13:34:44 -0800

    initial impl of handler fn and module

---
+++ src/core/handler.rs
@@ -0,0 +1,82 @@
+use crate::core::Context;
+use std::io::{Error, ErrorKind};
+
+pub fn handler<E, C, R, Handler: Fn(E, C) -> Result<R, std::io::Error>>(
+    handle: Handler,
+    event: E,
+    context: C,
+) -> Result<R, std::io::Error> {
+    let event_result = handle(event, context);
+    match event_result {
+        Ok(something) => Ok(something),
+        Err(error) => Err(Error::new(ErrorKind::Other, error)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn run_handler() -> Result<(), std::io::Error> {
+        struct TestRequest {
+            test_request: String,
+        }
+
+        struct TestResponse {
+            test_response: String,
+            test_context: Context,
+        }
+
+        let test_request = TestRequest {
+            test_request: String::from("hello"),
+        };
+
+        let test_aws_request_id = String::from("8476a536-e9f4-11e8-9739-2dfe598c3fcd");
+        let test_arn =
+            String::from("arn:aws:lambda:us-east-2:123456789012:function:custom-runtime");
+        let test_identity = String::from("test_identity");
+        let test_client_context = String::from("test_client_context");
+
+        let test_context = Context::create(
+            test_aws_request_id,
+            test_arn,
+            test_identity,
+            test_client_context,
+        )
+        .await;
+
+        fn test_handler_function(
+            event: TestRequest,
+            context: Context,
+        ) -> Result<TestResponse, std::io::Error> {
+            let response = TestResponse {
+                test_response: event.test_request,
+                test_context: context,
+            };
+            Ok(response)
+        }
+
+        let result = handler(test_handler_function, test_request, test_context);
+
+        if let Ok(test_kaon_result) = result {
+            assert_eq!(test_kaon_result.test_response, String::from("hello"));
+            assert_eq!(
+                test_kaon_result.test_context.aws_request_id,
+                String::from("8476a536-e9f4-11e8-9739-2dfe598c3fcd"),
+            );
+            assert_eq!(
+                test_kaon_result.test_context.invoked_function_arn,
+                String::from("arn:aws:lambda:us-east-2:123456789012:function:custom-runtime"),
+            );
+            assert_eq!(
+                test_kaon_result.test_context.identity,
+                String::from("test_identity"),
+            );
+            assert_eq!(
+                test_kaon_result.test_context.client_context,
+                String::from("test_client_context"),
+            );
+        }
+        Ok(())
+    }
+}
