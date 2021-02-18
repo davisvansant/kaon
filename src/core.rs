d9c7a81549404850607cd895dff8cc52a5b1767a
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-02-17 17:44:51 -0800

    minor adjustments to where constraints, adjust response body in loop, update tests

--- src/core.rs
+++ src/core.rs
@@ -58,10 +58,10 @@ impl Kaon {
 
     pub async fn decay<F, B, C, D>(&mut self, function: F)
     where
-        B: for<'de> Deserialize<'de> + Copy,
-        C: Serialize + Copy,
+        B: for<'de> Deserialize<'de>,
+        C: Serialize,
         F: Fn(B, Context) -> D,
-        D: Future<Output = Result<C, ()>> + Serialize,
+        D: Future<Output = Result<C, ()>>,
     {
         self.in_flight = true;
 
@@ -88,11 +88,13 @@ impl Kaon {
                 .await;
                 self.collect_event(context.clone()).await;
                 // checkpoint to see if we want to continue processing
-                let cloned_body = event_response.into_body();
-                let response_body = hyper::body::to_bytes(cloned_body).await.unwrap();
-                let response_json = serde_json::from_slice(&response_body).unwrap();
+                let response_body = event_response.into_body();
+                let response_body_bytes =
+                    hyper::body::to_bytes(response_body).await.unwrap().to_vec();
+
                 while self.in_flight {
                     // let fake_body = Body::from("more to come...");
+                    let response_json: B = serde_json::from_slice(&response_body_bytes).unwrap();
                     let handler_result = handler.run(response_json, context.clone()).await;
                     match handler_result {
                         Ok(result) => {
@@ -196,73 +198,110 @@ mod tests {
         assert_eq!(kaon.processed.is_empty(), true);
     }
 
-    // #[tokio::test]
-    // async fn decay() {
-    //     let test_aws_lambda_runtime_api = mockito::server_address().to_string();
-    //     std::env::set_var("AWS_LAMBDA_RUNTIME_API", test_aws_lambda_runtime_api);
-    //     let test_aws_request_id = String::from("8476a536-e9f4-11e8-9739-2dfe598c3fcd");
-    //     let test_arn =
-    //         String::from("arn:aws:lambda:us-east-2:123456789012:function:custom-runtime");
-    //     let test_identity = String::from("test_identity");
-    //     let test_client_context = String::from("test_client_context");
-    //
-    //     let test_context = Context::create(
-    //         test_aws_request_id,
-    //         test_arn,
-    //         test_identity,
-    //         test_client_context,
-    //     )
-    //     .await;
-    //     let test_body = Body::from("test");
-    //     let mock = mockito::mock("GET", "/2018-06-01/runtime/invocation/next")
-    //         .with_status(200)
-    //         .with_header("content-type", "application/json")
-    //         .with_header(
-    //             "Lambda-Runtime-Aws-Request-Id",
-    //             "8476a536-e9f4-11e8-9739-2dfe598c3fcd",
-    //         )
-    //         .with_header(
-    //             "Lambda-Runtime-Trace-Id",
-    //             "Root=1-5bef4de7-ad49b0e87f6ef6c87fc2e700;Parent=9a9197af755a6419;Sampled=1",
-    //         )
-    //         .with_header(
-    //             "Lambda-Runtime-Client-Context",
-    //             "test_aws_mobile_sdk_client_and_device",
-    //         )
-    //         .with_header(
-    //             "Lambda-Runtime-Cognito-Identity",
-    //             "test_aws_module_sdk_cognito_idp",
-    //         )
-    //         .with_header("Lambda-Runtime-Deadline-Ms", "1542409706888")
-    //         .with_header(
-    //             "Lambda-Runtime-Invoked-Function-Arn",
-    //             "arn:aws:lambda:us-east-2:123456789012:function:custom-runtime",
-    //         )
-    //         .with_body(r#"{"test": "kaon"}"#)
-    //         .expect(2)
-    //         .create();
-    //     let mock_post = mockito::mock(
-    //         "POST",
-    //         "/2018-06-01/runtime/invocation/8476a536-e9f4-11e8-9739-2dfe598c3fcd/response",
-    //     )
-    //     .match_body("more to come...")
-    //     .expect(1)
-    //     .create();
-    //
-    //     fn test_event(test_body: Body, test_context: Context) {
-    //         println!("kaon test {:?}", test_body);
-    //         println!("kaon test {}", test_context.aws_request_id);
-    //     }
-    //
-    //     let mut kaon = Kaon::charge().await;
-    //     assert_eq!(kaon.in_flight, false);
-    //
-    //     kaon.decay(test_event).await;
-    //     mock.assert();
-    //     assert!(mock.matched());
-    //     mock_post.assert();
-    //     assert!(mock_post.matched());
-    //     kaon.stop();
-    //     assert_eq!(kaon.in_flight, false);
-    // }
+    #[tokio::test]
+    async fn decay() {
+        let test_aws_lambda_runtime_api = mockito::server_address().to_string();
+        std::env::set_var("AWS_LAMBDA_RUNTIME_API", test_aws_lambda_runtime_api);
+        // let test_aws_request_id = String::from("8476a536-e9f4-11e8-9739-2dfe598c3fcd");
+        // let test_arn =
+        //     String::from("arn:aws:lambda:us-east-2:123456789012:function:custom-runtime");
+        // let test_identity = String::from("test_identity");
+        // let test_client_context = String::from("test_client_context");
+        //
+        // let test_context = Context::create(
+        //     test_aws_request_id,
+        //     test_arn,
+        //     test_identity,
+        //     test_client_context,
+        // )
+        // .await;
+        // let test_body = Body::from("test");
+        let mock = mockito::mock("GET", "/2018-06-01/runtime/invocation/next")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_header(
+                "Lambda-Runtime-Aws-Request-Id",
+                "8476a536-e9f4-11e8-9739-2dfe598c3fcd",
+            )
+            .with_header(
+                "Lambda-Runtime-Trace-Id",
+                "Root=1-5bef4de7-ad49b0e87f6ef6c87fc2e700;Parent=9a9197af755a6419;Sampled=1",
+            )
+            .with_header(
+                "Lambda-Runtime-Client-Context",
+                "test_aws_mobile_sdk_client_and_device",
+            )
+            .with_header(
+                "Lambda-Runtime-Cognito-Identity",
+                "test_aws_module_sdk_cognito_idp",
+            )
+            .with_header("Lambda-Runtime-Deadline-Ms", "1542409706888")
+            .with_header(
+                "Lambda-Runtime-Invoked-Function-Arn",
+                "arn:aws:lambda:us-east-2:123456789012:function:custom-runtime",
+            )
+            // .with_body(r#"{"test": "kaon"}"#)
+            .with_body(r#"{"test_request": "hello"}"#)
+            .expect(2)
+            .create();
+        let mock_post = mockito::mock(
+            "POST",
+            "/2018-06-01/runtime/invocation/8476a536-e9f4-11e8-9739-2dfe598c3fcd/response",
+        )
+        .match_body(r#"{"test_response":"hello"}"#)
+        // .with_body(r#"{"test_response": ""}"#)
+        .expect(1)
+        .create();
+
+        #[derive(Deserialize)]
+        struct TestRequest {
+            test_request: String,
+        }
+
+        #[derive(Serialize)]
+        struct TestResponse {
+            pub test_response: String,
+            // pub test_context: Context,
+        }
+
+        // let test_request = TestRequest {
+        //     test_request: String::from("hello"),
+        // };
+
+        // let test_aws_request_id = String::from("8476a536-e9f4-11e8-9739-2dfe598c3fcd");
+        // let test_arn =
+        //     String::from("arn:aws:lambda:us-east-2:123456789012:function:custom-runtime");
+        // let test_identity = String::from("test_identity");
+        // let test_client_context = String::from("test_client_context");
+        //
+        // let test_context = Context::create(
+        //     test_aws_request_id,
+        //     test_arn,
+        //     test_identity,
+        //     test_client_context,
+        // )
+        // .await;
+
+        async fn test_handler_function(
+            event: TestRequest,
+            _context: Context,
+        ) -> Result<TestResponse, ()> {
+            let response = TestResponse {
+                test_response: event.test_request,
+                // test_context: context,
+            };
+            Ok(response)
+        }
+
+        let mut kaon = Kaon::charge().await;
+        assert_eq!(kaon.in_flight, false);
+
+        kaon.decay(test_handler_function).await;
+        mock.assert();
+        assert!(mock.matched());
+        mock_post.assert();
+        assert!(mock_post.matched());
+        kaon.stop();
+        assert_eq!(kaon.in_flight, false);
+    }
 }
